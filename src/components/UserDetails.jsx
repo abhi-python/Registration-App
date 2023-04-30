5043ef74ae67a19a941ea0ca835eef0e26f236c3
abhi-python <abhilash.saini1@gmail.com>
2023-04-30 20:18:57 +0530

    sixth commit

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-import DataTable, { createTheme } from "react-data-table-component";
+import DataTable from "react-data-table-component";
+import { Link } from "react-router-dom";
 
 const UserDetails = () => {
   const [users, setUsers] = useState([]);
@@ -21,60 +22,20 @@ const UserDetails = () => {
       selector: (row) => row.sex,
     },
     {
-      name: "Mobile Number",
-      selector: (row) => row.mobile,
-    },
-    {
-      name: "Govt Id",
-      selector: (row) => row.govtId,
-    },
-    {
-      name: "Gaurdian Name",
-      selector: (row) => row.gaurdian_name,
-    },
-    {
-      name: "Email",
-      selector: (row) => row.email,
-    },
-    {
-      name: "Emergency Contact Number",
-      selector: (row) => row.emergency_contact_number,
+      name: "Mobile",
+      selector: (row) => row.mobile_number,
     },
     {
       name: "Address",
       selector: (row) => row.address,
     },
     {
-      name: "Country",
-      selector: (row) => row.country,
+      name: "Govt Id",
+      selector: (row) => row.govtId,
     },
     {
-      name: "State",
-      selector: (row) => row.state,
-    },
-    {
-      name: "City",
-      selector: (row) => row.city,
-    },
-    {
-      name: "Pincode",
-      selector: (row) => row.pincode,
-    },
-    {
-      name: "Occupation",
-      selector: (row) => row.occupation,
-    },
-    {
-      name: "Religion",
-      selector: (row) => row.religion,
-    },
-    {
-      name: "Marital Status",
-      selector: (row) => row.marital_status,
-    },
-    {
-      name: "Blood Group",
-      selector: (row) => row.blood_group,
+      name: "Gaurdian Details",
+      selector: (row) => row.gaurdian_name,
     },
     {
       name: "Nationality",
@@ -97,57 +58,27 @@ const UserDetails = () => {
     },
   };
 
-  // createTheme(
-  //   "solarized",
-  //   {
-  //     text: {
-  //       primary: "#268bd2",
-  //       secondary: "#2aa198",
-  //     },
-  //     background: {
-  //       default: "#002b36",
-  //     },
-  //     context: {
-  //       background: "#cb4b16",
-  //       text: "#FFFFFF",
-  //     },
-  //     divider: {
-  //       default: "#073642",
-  //     },
-  //     action: {
-  //       button: "rgba(0,0,0,.54)",
-  //       hover: "rgba(0,0,0,.08)",
-  //       disabled: "rgba(0,0,0,.12)",
-  //     },
-  //   },
-  //   "dark"
-  // );
-
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(userCollectionRef);
       setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.Id })));
     };
     getUsers();
-  }, []);
+  }, [userCollectionRef]);
 
   return (
     <div>
-      {/* <h1>User Details</h1> */}
-      {/* {users.map((user) => (
-        <div>
-          <h2>Name: {user.name}</h2>
-          <h3>Age: {user.age}</h3>
-        </div>
-      ))} */}
-
       <DataTable
         title="User List"
         columns={columns}
         data={users}
         customStyles={customStyles}
-        // theme="solarized"
       />
+      <Link to="/">
+        <button className="submit" style={{ marginTop: "20px" }}>
+          Go Back
+        </button>
+      </Link>
     </div>
   );
 };
--- src/components/UserRegistrationForm.jsx
+++ src/components/UserRegistrationForm.jsx
@@ -1,60 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Select from "react-select";
 import { Country, State, City } from "country-state-city";
 import { db } from "./firebase-config";
 import { collection, addDoc } from "firebase/firestore";
-import { Link } from "react-router-dom";
-import UserDetails from "./UserDetails";
+import { Link, useNavigate } from "react-router-dom";
 
 const UserRegistrationForm = () => {
-  const panReg =
-    /(^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$)|(^([0-9]){4}([a-zA-Z]){3}?$)/;
-  const adharReg = /^\d{12}$/;
-  const schema = yup
-    .object()
-    .shape({
-      name: yup.string().required("Please enter name"),
-      dob: yup.string().required("Date of birth required"),
-      sex: yup.string().required("Please select sex"),
-      mobileNumber: yup
-        .string()
-        .matches(
-          /^[6-9]\d{9}$/,
-          "Mobile number must be a valid Indian mobile number"
-        )
-        .nullable(),
+  const navigate = useNavigate();
+  const schema = yup.object().shape({
+    name: yup.string().required("Please enter name"),
+    dob: yup.string().required("Date of birth required"),
+    sex: yup.string().required("Please select sex"),
+    mobileNumber: yup
+      .string()
+      .matches(
+        /^[6-9]\d{9}$/,
+        "Mobile number must be a valid Indian mobile number"
+      )
+      .nullable(),
 
-      idType: yup.string().required("ID type is required"),
-      // id: yup.string().when("idType", {
-      //   is: "Aadhar",
-      //   then: yup
-      //     .string()
-      //     .matches(
-      //       /^\d{12}$/,
-      //       "Govt Id must be a valid 12-digit numeric string"
-      //     ),
-      //   otherwise: yup
-      //     .string()
-      //     .matches(
-      //       /^[A-Za-z]{5}\d{4}[A-Za-z]{1}$/,
-      //       "Govt Id must be a valid 10-digit alpha-numeric string"
-      //     ),
-      // }),
-      emergencyNumber: yup
-        .string()
-        .matches(
-          /^[6-9]\d{9}$/,
-          "Emergency Contact Number must be a valid Indian mobile number"
-        )
-        .nullable(),
+    idType: yup.string().required("ID type is required"),
 
-    });
+    emergencyNumber: yup
+      .string()
+      .matches(
+        /^[6-9]\d{9}$/,
+        "Emergency Contact Number must be a valid Indian mobile number"
+      )
+      .nullable(),
+  });
   const [countries, setCountries] = useState([]);
-  const [countryName, setCountryName] = useState("");
+  const [, setCountryName] = useState("");
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
   const userCollectionRef = collection(db, "users");
@@ -66,17 +45,17 @@ const UserRegistrationForm = () => {
     reset,
     watch,
   } = useForm({
-
     resolver: yupResolver(schema),
   });
   const onSubmit = async (data) => {
     console.log(data);
+    navigate("/userdetails");
     return addDoc(userCollectionRef, {
       name: data.name,
       age: data.dob,
       sex: data.sex,
       mobile_number: data.mobileNumber || "",
-      govtId: data.govtIdValue || "",
+      govtId: data.idType || "",
       gaurdian_name: data.gaurdianName || "",
       email: data.email || "",
       emergency_contact_number: data.emergencyNumber || "",
@@ -107,7 +86,7 @@ const UserRegistrationForm = () => {
     console.log("country", selectedCountry.name);
     setCountryName(selectedCountry.name);
     let states = State.getStatesOfCountry(selectedCountry.value);
-    if (states.length == 0) {
+    if (states.length === 0) {
       states = State.getStatesOfCountry(selectedCountry.isoCode);
     }
     setStates(
@@ -119,28 +98,19 @@ const UserRegistrationForm = () => {
     );
     setCities([]);
   };
-  // console.log("tabke",countries.name);
-  // console.log("stateTable", states);
+
   const handleStateChange = (selectedState) => {
-    // console.log("state", selectedState);
-    // console.log(City.getCitiesOfCountry("IN"));
-    // console.log(City.getCitiesOfState(selectedState.isoCode,selectedState.value));
     const cities = City.getCitiesOfState(
       selectedState.isoCode,
       selectedState.value
     );
-    // console.log("cities",newCities);
+
     setCities(cities.map((city) => ({ label: city.name, value: city.name })));
     console.log("city", cities);
-    // console.log(selectedState)
   };
-  // console.log("city",cities);
-  // console.table(countries)
-  const handleCityChange = (selectedCity, selectedState) => {};
 
-  // const { values, setValues} = useFormState({
-  //   control
-  // });
+  const handleCityChange = () => {};
+
   const addRecord = async () => {};
   return (
     <div className="user-form">
@@ -181,7 +151,7 @@ const UserRegistrationForm = () => {
               Sex<sup className="required">*</sup>
             </label>
             <select {...register("sex", { required: true })}>
-              <option value="">Select Sex</option>
+              <option value="">---Select Sex---</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="others">Others</option>
@@ -308,17 +278,12 @@ const UserRegistrationForm = () => {
             <label>Country</label>
             <Select
               {...register("country", { required: true })}
-              // const selectedCountry = watch("country");
               id="country"
               name="country"
               label="country"
               options={countries}
               value={watch("country")}
               onChange={handleCountryChange}
-
-              //     => {
-              //   setValues({ country: value, state: null, city: null }, false);
-              // }}
             />
           </div>
           <div className="input-field">
@@ -344,7 +309,7 @@ const UserRegistrationForm = () => {
           <div className="input-field">
             <label>Religion</label>
             <select {...register("religion", { required: true })}>
-              <option value="">Select Religion</option>
+              <option value="">---Select Religion---</option>
               <option value="hindu">Hindu</option>
               <option value="muslim">Muslim</option>
               <option value="crishtian">Crishtian</option>
@@ -353,7 +318,7 @@ const UserRegistrationForm = () => {
           <div className="input-field">
             <label>Marital Status</label>
             <select {...register("maritalStatus", { required: true })}>
-              <option value="">Select Marital Status</option>
+              <option value="">---Select Marital Status---</option>
               <option value="married">Married</option>
               <option value="unmarried">Unmarried</option>
             </select>
@@ -361,7 +326,7 @@ const UserRegistrationForm = () => {
           <div className="input-field">
             <label>Blood Group</label>
             <select {...register("bloodGroup", { required: true })}>
-              <option value="">Select Group</option>
+              <option value="">---Select Group---</option>
               <option value="A+">A+</option>
               <option value="B+">B+</option>
               <option value="O+">O+</option>
@@ -376,17 +341,206 @@ const UserRegistrationForm = () => {
         <div className="input-field">
           <label>Nationality</label>
           <select {...register("nationality", { required: true })}>
-            <option value="">Select Nationality</option>
-            <option value="inidan">Inidan</option>
-            <option value="bangaldeshi">Bangladesi</option>
+            <option value="">---Select Nationality---</option>
+            <option value="afghan">Afghan</option>
+            <option value="albanian">Albanian</option>
+            <option value="algerian">Algerian</option>
+            <option value="american">American</option>
+            <option value="andorran">Andorran</option>
+            <option value="angolan">Angolan</option>
+            <option value="antiguans">Antiguans</option>
+            <option value="argentinean">Argentinean</option>
+            <option value="armenian">Armenian</option>
+            <option value="australian">Australian</option>
+            <option value="austrian">Austrian</option>
+            <option value="azerbaijani">Azerbaijani</option>
+            <option value="bahamian">Bahamian</option>
+            <option value="bahraini">Bahraini</option>
+            <option value="bangladeshi">Bangladeshi</option>
+            <option value="barbadian">Barbadian</option>
+            <option value="barbudans">Barbudans</option>
+            <option value="batswana">Batswana</option>
+            <option value="belarusian">Belarusian</option>
+            <option value="belgian">Belgian</option>
+            <option value="belizean">Belizean</option>
+            <option value="beninese">Beninese</option>
+            <option value="bhutanese">Bhutanese</option>
+            <option value="bolivian">Bolivian</option>
+            <option value="bosnian">Bosnian</option>
+            <option value="brazilian">Brazilian</option>
+            <option value="british">British</option>
+            <option value="bruneian">Bruneian</option>
+            <option value="bulgarian">Bulgarian</option>
+            <option value="burkinabe">Burkinabe</option>
+            <option value="burmese">Burmese</option>
+            <option value="burundian">Burundian</option>
+            <option value="cambodian">Cambodian</option>
+            <option value="cameroonian">Cameroonian</option>
+            <option value="canadian">Canadian</option>
+            <option value="cape verdean">Cape Verdean</option>
+            <option value="central african">Central African</option>
+            <option value="chadian">Chadian</option>
+            <option value="chilean">Chilean</option>
+            <option value="chinese">Chinese</option>
+            <option value="colombian">Colombian</option>
+            <option value="comoran">Comoran</option>
+            <option value="congolese">Congolese</option>
+            <option value="costa rican">Costa Rican</option>
+            <option value="croatian">Croatian</option>
+            <option value="cuban">Cuban</option>
+            <option value="cypriot">Cypriot</option>
+            <option value="czech">Czech</option>
+            <option value="danish">Danish</option>
+            <option value="djibouti">Djibouti</option>
+            <option value="dominican">Dominican</option>
+            <option value="dutch">Dutch</option>
+            <option value="east timorese">East Timorese</option>
+            <option value="ecuadorean">Ecuadorean</option>
+            <option value="egyptian">Egyptian</option>
+            <option value="emirian">Emirian</option>
+            <option value="equatorial guinean">Equatorial Guinean</option>
+            <option value="eritrean">Eritrean</option>
+            <option value="estonian">Estonian</option>
+            <option value="ethiopian">Ethiopian</option>
+            <option value="fijian">Fijian</option>
+            <option value="filipino">Filipino</option>
+            <option value="finnish">Finnish</option>
+            <option value="french">French</option>
+            <option value="gabonese">Gabonese</option>
+            <option value="gambian">Gambian</option>
+            <option value="georgian">Georgian</option>
+            <option value="german">German</option>
+            <option value="ghanaian">Ghanaian</option>
+            <option value="greek">Greek</option>
+            <option value="grenadian">Grenadian</option>
+            <option value="guatemalan">Guatemalan</option>
+            <option value="guinea-bissauan">Guinea-Bissauan</option>
+            <option value="guinean">Guinean</option>
+            <option value="guyanese">Guyanese</option>
+            <option value="haitian">Haitian</option>
+            <option value="herzegovinian">Herzegovinian</option>
+            <option value="honduran">Honduran</option>
+            <option value="hungarian">Hungarian</option>
+            <option value="icelander">Icelander</option>
+            <option value="indian">Indian</option>
+            <option value="indonesian">Indonesian</option>
+            <option value="iranian">Iranian</option>
+            <option value="iraqi">Iraqi</option>
+            <option value="irish">Irish</option>
+            <option value="israeli">Israeli</option>
+            <option value="italian">Italian</option>
+            <option value="ivorian">Ivorian</option>
+            <option value="jamaican">Jamaican</option>
+            <option value="japanese">Japanese</option>
+            <option value="jordanian">Jordanian</option>
+            <option value="kazakhstani">Kazakhstani</option>
+            <option value="kenyan">Kenyan</option>
+            <option value="kittian and nevisian">Kittian and Nevisian</option>
+            <option value="kuwaiti">Kuwaiti</option>
+            <option value="kyrgyz">Kyrgyz</option>
+            <option value="laotian">Laotian</option>
+            <option value="latvian">Latvian</option>
+            <option value="lebanese">Lebanese</option>
+            <option value="liberian">Liberian</option>
+            <option value="libyan">Libyan</option>
+            <option value="liechtensteiner">Liechtensteiner</option>
+            <option value="lithuanian">Lithuanian</option>
+            <option value="luxembourger">Luxembourger</option>
+            <option value="macedonian">Macedonian</option>
+            <option value="malagasy">Malagasy</option>
+            <option value="malawian">Malawian</option>
+            <option value="malaysian">Malaysian</option>
+            <option value="maldivan">Maldivan</option>
+            <option value="malian">Malian</option>
+            <option value="maltese">Maltese</option>
+            <option value="marshallese">Marshallese</option>
+            <option value="mauritanian">Mauritanian</option>
+            <option value="mauritian">Mauritian</option>
+            <option value="mexican">Mexican</option>
+            <option value="micronesian">Micronesian</option>
+            <option value="moldovan">Moldovan</option>
+            <option value="monacan">Monacan</option>
+            <option value="mongolian">Mongolian</option>
+            <option value="moroccan">Moroccan</option>
+            <option value="mosotho">Mosotho</option>
+            <option value="motswana">Motswana</option>
+            <option value="mozambican">Mozambican</option>
+            <option value="namibian">Namibian</option>
+            <option value="nauruan">Nauruan</option>
+            <option value="nepalese">Nepalese</option>
+            <option value="new zealander">New Zealander</option>
+            <option value="ni-vanuatu">Ni-Vanuatu</option>
+            <option value="nicaraguan">Nicaraguan</option>
+            <option value="nigerien">Nigerien</option>
+            <option value="north korean">North Korean</option>
+            <option value="northern irish">Northern Irish</option>
+            <option value="norwegian">Norwegian</option>
+            <option value="omani">Omani</option>
+            <option value="pakistani">Pakistani</option>
+            <option value="palauan">Palauan</option>
+            <option value="panamanian">Panamanian</option>
+            <option value="papua new guinean">Papua New Guinean</option>
+            <option value="paraguayan">Paraguayan</option>
+            <option value="peruvian">Peruvian</option>
+            <option value="polish">Polish</option>
+            <option value="portuguese">Portuguese</option>
+            <option value="qatari">Qatari</option>
+            <option value="romanian">Romanian</option>
             <option value="russian">Russian</option>
+            <option value="rwandan">Rwandan</option>
+            <option value="saint lucian">Saint Lucian</option>
+            <option value="salvadoran">Salvadoran</option>
+            <option value="samoan">Samoan</option>
+            <option value="san marinese">San Marinese</option>
+            <option value="sao tomean">Sao Tomean</option>
+            <option value="saudi">Saudi</option>
+            <option value="scottish">Scottish</option>
+            <option value="senegalese">Senegalese</option>
+            <option value="serbian">Serbian</option>
+            <option value="seychellois">Seychellois</option>
+            <option value="sierra leonean">Sierra Leonean</option>
+            <option value="singaporean">Singaporean</option>
+            <option value="slovakian">Slovakian</option>
+            <option value="slovenian">Slovenian</option>
+            <option value="solomon islander">Solomon Islander</option>
+            <option value="somali">Somali</option>
+            <option value="south african">South African</option>
+            <option value="south korean">South Korean</option>
+            <option value="spanish">Spanish</option>
+            <option value="sri lankan">Sri Lankan</option>
+            <option value="sudanese">Sudanese</option>
+            <option value="surinamer">Surinamer</option>
+            <option value="swazi">Swazi</option>
+            <option value="swedish">Swedish</option>
+            <option value="swiss">Swiss</option>
+            <option value="syrian">Syrian</option>
+            <option value="taiwanese">Taiwanese</option>
+            <option value="tajik">Tajik</option>
+            <option value="tanzanian">Tanzanian</option>
+            <option value="thai">Thai</option>
+            <option value="togolese">Togolese</option>
+            <option value="tongan">Tongan</option>
+            <option value="trinidadian or tobagonian">
+              Trinidadian or Tobagonian
+            </option>
+            <option value="tunisian">Tunisian</option>
+            <option value="turkish">Turkish</option>
+            <option value="tuvaluan">Tuvaluan</option>
+            <option value="ugandan">Ugandan</option>
+            <option value="ukrainian">Ukrainian</option>
+            <option value="uruguayan">Uruguayan</option>
+            <option value="uzbekistani">Uzbekistani</option>
+            <option value="venezuelan">Venezuelan</option>
+            <option value="vietnamese">Vietnamese</option>
+            <option value="welsh">Welsh</option>
+            <option value="yemenite">Yemenite</option>
+            <option value="zambian">Zambian</option>
+            <option value="zimbabwean">Zimbabwean</option>
           </select>
         </div>
         <div className="buttons">
-          <Link to='/userdetails'>
-            <button className="submit">
-              Show Data
-            </button>
+          <Link to="/userdetails">
+            <button className="submit">Show Data</button>
           </Link>
           <button className="delete" onClick={() => reset()}>
             Cancel
